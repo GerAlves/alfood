625ba0f654314b33a933cded8c90c5859d5508de
Vinicios Neves <vinny@macbook.lan>
2022-03-16 13:59:27 +0000

    versao inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
-import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
-import FormularioPrato from './paginas/Administracao/Pratos/FormularioPrato';
-import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
-import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 
@@ -13,17 +8,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-
-      <Route path='/admin' element={<PaginaBaseAdmin />}>
-
-        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
-        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
-        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
-
-        <Route path="pratos" element={<AdministracaoPratos />} />
-        <Route path="pratos/novo" element={<FormularioPrato />} />
-      </Route>
-
     </Routes>
   );
 }
--- src/interfaces/ITag.ts
+++
@@ -1,4 +0,0 @@
-export default interface ITag {
-    value: string
-    id: number
-}
