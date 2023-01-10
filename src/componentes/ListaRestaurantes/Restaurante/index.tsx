b33feb387fac85a27ffaf5cc06736b522b1a3f7a
Gerson Alves <alves.ger95@gmail.com>
2023-01-10 11:36:11 -0400

    update

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,19 +1,30 @@
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import { useState, useEffect } from 'react'; 
+import IPrato from '../../../interfaces/IPrato';
+import axios from 'axios';
 
 interface RestauranteProps {
   restaurante: IRestaurante
 }
 
-const Restaurante = ({ restaurante }: RestauranteProps) => {
+const Restaurante = ({ restaurante } : RestauranteProps) => {
+  const [pratos, setPratos] = useState<IPrato[]>()
+  useEffect(() => {
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+    .then(response => {
+      setPratos(response.data)
+    })
+  }, [restaurante.id])
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      
     </div>
   </section>)
 }
